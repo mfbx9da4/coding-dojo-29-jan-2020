01177bd3f56b9785d1632a4ed05d4de4a8fca098
David Adler <dalberto.adler@gmail.com>
2020-01-29 20:18:47 +0000

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,44 @@
-export default class Robot {
-  private output: number[][];
+export class Robot {
+  private slots: number[][];
+  private blockToSlotIndices: Array<number>;
   constructor(n: number) {
-    this.output = [];
+    this.slots = [];
+    this.blockToSlotIndices = [];
     for (let i = 0; i < n; i++) {
-      this.output.push([i]);
+      this.slots.push([i]);
+      this.blockToSlotIndices.push(i);
     }
   }
 
   getOutput(): number[][] {
-    return this.output;
+    return this.slots;
+  }
+
+  private getSlot(a: number): number {
+    return this.blockToSlotIndices[a];
+  }
+
+  private remove(a: number): Array<number> {
+    const slot = this.getSlot(a);
+    const start = this.slots[slot].indexOf(a);
+    return this.slots[slot].splice(start);
+  }
+
+  private putOver(newStack: number[], slotIndex: number): void {
+    for (let i = 0; i < newStack.length; i++) {
+      const block = newStack[i];
+      this.blockToSlotIndices[block] = slotIndex;
+      this.slots[slotIndex].push(block);
+    }
+  }
+
+  pileOver(a: number, b: number): void {
+    const stackA = this.remove(a);
+    const slotIndex = this.getSlot(b);
+    this.putOver(stackA, slotIndex);
   }
 }
+
+// export function main(input: string) {
+//   const testCases = input.split("\n");
+// }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,24 @@
-import Robot from "../src/index";
-import _ from 'lodash';
+import { Robot } from "../src/index";
+import _ from "lodash";
+
+const equal = (a, b) => expect(_.isEqual(a, b));
 
 describe("name", () => {
-    it("doesn't crash when constructing", () => {
+  it("doesn't crash when constructing", () => {
+    new Robot(3);
+  });
+  it("blocks should remain without operations", () => {
+    const robot = new Robot(3);
+    equal(robot.getOutput(), [[0], [1], [2]]);
+  });
+
+  describe("pile onto", () => {});
+  describe("pile over", () => {
+    it("one operation", () => {
       const robot = new Robot(3);
+      robot.pileOver(1, 2);
+      equal(robot.getOutput(), [[0], [], [2, 1]]);
     });
-    it("blocks should remain without operations", () => {
-        const robot = new Robot(3)
-        expect(_.isEqual(robot.getOutput(),[[0], [1], [2]])).toBe(true);
-    });
+  });
+  describe("move over", () => {});
 });
